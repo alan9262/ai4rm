1eb0287aecb8d3a685bde13b01aff3fd5289dcc1
Sherylhs <sherrybear1280@gmail.com>
2020-04-29 19:04:29 -0400

    add filter look to the clusters

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import Tabular from './Tabular';
-import RFTable from './RFTable';
-import MatrixTable from './MatrixTable';
 import HistoChart from './HistoChart';
 import StackedColumnChart from './StackedColumnChart';
 import BarChart from './BarChart';
@@ -13,7 +10,10 @@ export default class Analytics extends Component {
     state = {
         timeBasedProducts: [],
         confMatrix: [],
-        clusters: []
+        clusters: [],
+        wasOpened1: Boolean,
+        wasOpened2: Boolean,
+        wasOpened3: Boolean,
     }
 
     accuracy(acc) {
@@ -47,14 +47,33 @@ export default class Analytics extends Component {
             cluster.push("Top" + " " + row.channel + " " + row.product_1 + " buyers")
         });
         this.setState({
-            clusters: cluster
+            clusters: cluster,
+            wasOpened1: false,
+            wasOpened2: false,
+            wasOpened3: false,
         })
 
     }
 
-    
+
+
+    showDiv1() {
+        this.setState({ wasOpened1: !this.state.wasOpened1 })
+    }
+    showDiv2() {
+        this.setState({ wasOpened2: !this.state.wasOpened2 })
+    }
+    showDiv3() {
+        this.setState({ wasOpened3: !this.state.wasOpened3 })
+    }
+
+
 
     render() {
+        const wasOpened1 = this.state.wasOpened1;
+        const wasOpened2 = this.state.wasOpened2;
+        const wasOpened3 = this.state.wasOpened3;
+
         const { data, headers, stackedProducts, feature, view, role } = this.props;
         const { confMatrix, timeBasedProducts } = this.state;
         let dataRow = data.reduce(function (prev, current) {
@@ -63,38 +82,82 @@ export default class Analytics extends Component {
         // dataRow[0].predict_conver = Math.round(parseInt(dataRow[0].predict_conver)) * 100;
         let headings = ["Age", "Interest Channel", "Gender", "Session duration", "Season", "Product Category", "Quantity",
             "Clicks", "Impression"];
+
+        console.log(this.wasOpened)
         return (
             <div>
                 <br></br>
                 <Alert key={1} variant="primary">What should you know about targeting customers?</Alert><br></br>
-
+                <h2>Click to see the clusters:</h2>
                 <br></br>
-                <div className="label-div">
-                    <div>
-                    </div><br></br>
-                    <div className="col">
-                        <div className="row flex-nowrap">
+                <div id="myBtnContainer">
+                    <button className="btn" onClick={() => this.showDiv1()} > Cluster 1</button>
+                    <button className="btn" onClick={() => this.showDiv2()} > Cluster 2</button>
+                    <button className="btn" onClick={() => this.showDiv3()} > Cluster 3</button>
+                </div>
+                <div class="container">
+                    {wasOpened1 && (
+                        <div className="row flex-nowrap" id="filterDiv1" >
                             <div className="card-label">
                                 <h5 className="card-label-text">Right age for users</h5>
-                                <h1 style={{ textAlign: "center", color: "green" }}><br></br>{dataRow.age}</h1>
+                                <h1 style={{ textAlign: "center", color: "green" }}><br></br>{data[0].age}</h1>
                             </div>
                             <div className="card-label">
                                 <h5 className="card-label-text">Right Content for users</h5>
-                                <h1 style={{ textAlign: "center", color: "green" }}><br></br>{dataRow.product_1}</h1>
+                                <h1 style={{ textAlign: "center", color: "green" }}><br></br>{data[0].product_1}</h1>
                             </div>
                             <div className="card-label">
-                                <h5 className="card-label-text">Right Channel for users</h5> <h1 style={{ textAlign: "center", color: "green" }}><br></br>{dataRow.channel}</h1>
+                                <h5 className="card-label-text">Right Channel for users</h5> <h1 style={{ textAlign: "center", color: "green" }}><br></br>{data[0].channel}</h1>
                             </div>
                             <div className="card-label">
-                                <h5 className="card-label-text">Right Time for users</h5> <h1 style={{ textAlign: "center", color: "green" }}><br></br>{dataRow.time}</h1>
+                                <h5 className="card-label-text">Right Time for users</h5> <h1 style={{ textAlign: "center", color: "green" }}><br></br>{data[0].time}</h1>
                             </div>
                             <div className="card-label">
-                                <h5 className="card-label-text">Prediction accuracy</h5> <h1 style={{ textAlign: "center", color: "green" }}><br></br>{(dataRow.predict_conver * 100).toFixed(0)}%</h1>
+                                <h5 className="card-label-text">Prediction accuracy</h5> <h1 style={{ textAlign: "center", color: "green" }}><br></br>{(data[0].predict_conver * 100).toFixed(0)}%</h1>
                             </div>
                         </div>
-
-                    </div>
-                </div><hr></hr>
+                    )}
+                    {wasOpened2 && (
+                        <div className="row flex-nowrap" id="filterDiv2">
+                            <div className="card-label">
+                                <h5 className="card-label-text">Right age for users</h5>
+                                <h1 style={{ textAlign: "center", color: "green" }}><br></br>{data[1].age}</h1>
+                            </div>
+                            <div className="card-label">
+                                <h5 className="card-label-text">Right Content for users</h5>
+                                <h1 style={{ textAlign: "center", color: "green" }}><br></br>{data[1].product_1}</h1>
+                            </div>
+                            <div className="card-label">
+                                <h5 className="card-label-text">Right Channel for users</h5> <h1 style={{ textAlign: "center", color: "green" }}><br></br>{data[1].channel}</h1>
+                            </div>
+                            <div className="card-label">
+                                <h5 className="card-label-text">Right Time for users</h5> <h1 style={{ textAlign: "center", color: "green" }}><br></br>{data[1].time}</h1>
+                            </div>
+                            <div className="card-label">
+                                <h5 className="card-label-text">Prediction accuracy</h5> <h1 style={{ textAlign: "center", color: "green" }}><br></br>{(data[1].predict_conver * 100).toFixed(0)}%</h1>
+                            </div>
+                        </div>)}
+                    {wasOpened3 && (
+                        <div className="row flex-nowrap" id="filterDiv3">
+                            <div className="card-label">
+                                <h5 className="card-label-text">Right age for users</h5>
+                                <h1 style={{ textAlign: "center", color: "green" }}><br></br>{data[2].age}</h1>
+                            </div>
+                            <div className="card-label">
+                                <h5 className="card-label-text">Right Content for users</h5>
+                                <h1 style={{ textAlign: "center", color: "green" }}><br></br>{data[2].product_1}</h1>
+                            </div>
+                            <div className="card-label">
+                                <h5 className="card-label-text">Right Channel for users</h5> <h1 style={{ textAlign: "center", color: "green" }}><br></br>{data[2].channel}</h1>
+                            </div>
+                            <div className="card-label">
+                                <h5 className="card-label-text">Right Time for users</h5> <h1 style={{ textAlign: "center", color: "green" }}><br></br>{data[2].time}</h1>
+                            </div>
+                            <div className="card-label">
+                                <h5 className="card-label-text">Prediction accuracy</h5> <h1 style={{ textAlign: "center", color: "green" }}><br></br>{(data[2].predict_conver * 100).toFixed(0)}%</h1>
+                            </div>
+                        </div>)}
+                </div>
 
 
                 <Row>
@@ -129,7 +192,7 @@ export default class Analytics extends Component {
 
                     
                 </div><br></br><hr></hr> */}
-                
+
             </div>
         )
     }
